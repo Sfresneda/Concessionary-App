03c74b1c8c3472e52f0eb4e9398ee0efb689d04b
fresneda <fresneda@fresnedas-MacBook-Pro.local>
2019-02-05 21:57:25 +0100

    Added UITest for CatalogList

--- Concessionary App VIPER 2/ConcessionaryAppVIPERUITests/ConcessionaryAppVIPERUITests.swift
+++ Concessionary App VIPER 2/ConcessionaryAppVIPERUITests/ConcessionaryAppVIPERUITests.swift
@@ -8,12 +8,87 @@
 
 import XCTest
 
+extension BrandEnum: CaseIterable{}
+enum BrandEnum: String {
+    case audi = "Audi"
+    case mercedes = "Mercedes"
+    case volvo = "Volvo"
+}
+
 class ConcessionaryAppVIPERUITests: XCTestCase {
 
+    
+    let tableViewLabel: String = "Cars TableView"
+    let carCellLabel: String = "Car Cell"
+    
+    let selectBrandButtonLabel: String = "Select Brand Button"
+    let sortCarsButtonLabel: String = "Sort Button"
+    
+    let acceptBrandButtonLabel: String = "Accept Brand Button"
+    let brandPickerLabel: String = "Brand Picker"
+    
+    let k_load_timeout: Double = 3.5
+    
     override func setUp() {
         continueAfterFailure = false
         XCUIApplication().launch()
     }
 
     override func tearDown() {}
+    
+    // MARK: - Catalog List Test
+    func test_list_fillTableView(){
+        let app = XCUIApplication()
+        self.helper_fillTableView(app: app)
+        
+        let predicate = NSPredicate(format: "exists == true")
+        let query = app.tables[self.tableViewLabel].cells[self.carCellLabel]
+        self.helper_wait(for: query, with: predicate)
+        
+        XCTAssert(query.exists)
+    }
+    
+    func test_list_fillTableViewAndCheckCell(){
+        let app = XCUIApplication()
+        self.helper_fillTableView(app: app)
+        
+        let predicate = NSPredicate(format: "exists == true")
+        let query = app.tables[self.tableViewLabel].cells[self.carCellLabel].staticTexts["Audi V40"]
+        self.helper_wait(for: query, with: predicate)
+
+        XCTAssert(query.exists)
+    }
+    
+    func test_list_fillTableViewWithAllBrands() {
+        let app = XCUIApplication()
+        var exist: [Bool] = Array()
+        
+        for brand in BrandEnum.allCases {
+            self.helper_fillTableView(app: app, brandOption: brand.rawValue)
+            
+            let predicate = NSPredicate(format: "exists == true")
+            let query = app.tables[self.tableViewLabel].cells[self.carCellLabel].staticTexts["\(brand.rawValue) V40"]
+            self.helper_wait(for: query, with: predicate)
+            
+            if query.exists {
+                exist.append(query.exists)
+            }
+        }
+        XCTAssert(exist.filter{ value in value}.count == BrandEnum.allCases.count)
+    }
+   
+
+    // MARK: - Helpers
+    func helper_fillTableView(app: XCUIApplication, brandOption: String? = nil){
+        app.buttons[self.selectBrandButtonLabel].tap()
+        if let brand = brandOption {
+            app.pickerWheels.element.adjust(toPickerWheelValue: brand)
+        }
+        app.buttons[self.acceptBrandButtonLabel].tap()
+    }
+    
+    func helper_wait(for query: Any, with predicate: NSPredicate){
+        expectation(for: predicate, evaluatedWith: query, handler: nil)
+        waitForExpectations(timeout: self.k_load_timeout, handler: nil)
+    }
 }
